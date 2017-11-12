b38233a3cde8cc25cef352dd762ad2e94e31e36c
ward <vakpko@gmail.com>
2017-11-12 14:46:42 +0100

    creating song upp data seeding

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,5 +6,44 @@ def show
 def index
     @artists = Artist.all
   end
+  def new
+     @artist = Artist.new
+   end
 
+   def create
+     @artist = Artist.new(artist_params)
+     if @artist.save
+       pictures_params.each do |picture|
+         @artist.pictures.create(image_url: picture)
+       end
+       redirect_to new_artist_song_path(@artist), notice: "Artist created, you can now add songs"
+     else
+       render :new
+     end
+   end
+
+   def destroy
+     artist = set_artist
+     if !artist.core_artist?
+       artist.destroy
+       redirect_to root_path, notice: "Artist destroyed"
+     else
+       flash[:alert] = "Core artists cannot be destroyed, you can try to destroy other artists"
+       render :show
+     end
+   end
+
+   private
+
+   def set_artist
+     @artist = Artist.find(params[:id])
+   end
+
+   def artist_params
+     params.require(:artist).permit(:name, :active_since)
+   end
+
+   def pictures_params
+     params[:pictures].present? ? params.require(:pictures) : []
+   end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,39 +1,59 @@
 class SongsController < ApplicationController
   def show
-     @songs= Song.find(params[:id])
-   end
-
-  def index
-    @songs = artist.songs
-  end
-
-
-  def new
-    @song = artist.songs.build
-  end
-
-  def create
-    @song = artist.songs.build(song_params)
-
-    if @song.save
-      redirect_to @song, notice: "Song created"
-    else
-      render :new
+      @artist=set_artist
+      @song = set_song
+      # @duration = @song.convert_to_time
     end
-  end
+    def new
+      @song = set_artist.songs.build
+    end
 
+    def create
+      @song = set_artist.songs.build(song_params)
 
+      if @song.save
+        redirect_to artist_path(set_artist), notice: "Song created"
+      else
+        render :new
+      end
+    end
 
-  def set_song
-    @song = Song.find(params[:id])
-  end
+    def edit
+      @song = set_song
+      @artist = set_artist
+    end
 
-  def song_params
-    params.require(:song)
-      # .permit(
-      #   :home_type, :room_type, :accommodate, :bedroom_count, :bathroom_count, :listing_name,
-      #   :description, :address, :has_tv, :has_kitchen, :has_airco, :has_heating, :has_internet,
-      #   :price, :active
-      # )
-  end
+    def update
+      if set_song.update(song_params)
+        redirect_to artist_path(set_artist), notice: "Song updated"
+      else
+        render :edit #needs fixing
+      end
+    end
+
+    def destroy
+      song = set_song
+      song.destroy
+      redirect_to artist_path(set_artist), notice: "Song deleted"
+    end
+    # def convert_to_time
+    #   minutes = (@song.length / 60) % 60
+    #   seconds = @song.length % 60
+    #   format("%02d:%02d", minutes, seconds)
+    # end
+
+    private
+    def set_song
+      @song=Song.find(params[:id])
+    end
+
+    def set_artist
+      @artist = Artist.find(params[:artist_id])
+    end
+
+    def song_params
+      params
+      .require(:song)
+      .permit(:name, :length, :release_date)
+    end
 end
