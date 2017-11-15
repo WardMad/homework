71ad5a93569ba1d562df494ec3f2441169beeb7d
ward <vakpko@gmail.com>
2017-11-15 22:43:04 +0100

    heroku deploy

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  has_many :songs
-  has_many :photos
+  has_many :songs, dependent: :destroy
+  has_many :photos, dependent: :destroy
   validates :name, presence: true
 end
